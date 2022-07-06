5b30fe0e3fb42adc19c3565c40518c7cb6512ea7
Sebastien Neyt <sneyt@fu-r7-p4.s19.be>
2022-07-06 08:27:11 +0200

    bug

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sneyt <sneyt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 16:30:49 by sneyt             #+#    #+#             */
-/*   Updated: 2022/07/04 18:33:18 by sneyt            ###   ########.fr       */
+/*   Updated: 2022/07/05 18:09:11 by sneyt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,20 +89,14 @@ int	count_chunk_size(t_stack *stack)
 	int x;
 	x = 0;
 	i = stack->chunk_count - 1;
-	//printf(YELLOW"###chunk_number == %d######################\n"RESET_COLOR, stack->chunk_number);
+	
 	if (stack->chunk_count == 0)
 		return (0);
-	//else if (stack->chunk_count == 1)
-	//{
-		//stack->chunk_number--;
-//		return (1);
-//	}
 	else if (stack->chunk_number != 0)
 	{	
 		while (stack->chunk_number - 1 == stack->chunks[i] && i >= 0)
 		{
 		
-			//printf("%d \n", stack->chunks[i]);
 			i--;
 			x++;
 		}
@@ -423,6 +417,44 @@ int copy_arr(int *temp, t_stack *stack_a)
 	}
 	return (1);
 }
+int find_midpoint_chunk2(t_stack *stack, int resting_chunksize)
+{
+	int	temp[resting_chunksize];
+	int	value;
+	int	i;
+	int	a = 0;
+	int x = 0;
+	i = stack->count - 1;
+	//printf("this is the CHUNKSIZE; %d\n", chunksize);
+	while (x < resting_chunksize)
+	{
+		temp[x] = stack->arr[i];
+		i--;
+		x++;
+	}
+	printf("%d %d %d\n", temp[0], temp[1], temp[2]);	
+	
+	x = resting_chunksize;
+	
+	while (a < x)
+	{
+		i = 0;
+		while (i < x - 1)
+		{
+			if (temp[i] > temp[i + 1])
+			{
+				value = temp[i + 1];
+				temp[i + 1] = temp[i];
+				temp[i] = value;
+			}
+			i++;
+		}
+		a++;
+	}
+	value = temp[resting_chunksize / 2 ];
+	printf("%d %d %d %d %d %d \n", temp[0], temp[1], temp[2], temp[3], temp[4], temp[5]);	
+	return (value);
+}
 
 int	find_midpoint_chunk(t_stack *stack, int chunksize)
 {
@@ -461,6 +493,13 @@ int	find_midpoint_chunk(t_stack *stack, int chunksize)
 	value = temp[chunksize / 2 ];
 	printf("%d %d %d %d %d %d \n", temp[0], temp[1], temp[2], temp[3], temp[4], temp[5]);	
 	return (value);
+}
+
+int	check_top2(t_stack *stack)
+{
+	if (stack->arr[stack->count - 1] > stack->arr[stack->count - 2])
+		return (1);
+	return (0);
 }
 
 int		find_midpoint(t_stack *stack_a)
@@ -512,6 +551,22 @@ int	check_if_smaller(t_stack *stack, int midvalue)
 	return (0);
 }
 
+int	check_if_bigger(t_stack *stack_b, int midvalue, int chunksize)
+{
+	int	i = 0;
+	while (i < chunksize)  // 0 < 2
+	{
+		if (stack_b->arr[stack_b->count - 1 - i] > midvalue)
+		{
+			usleep(100000);
+			printf("found bigger one!!\n");
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
 int		midpointsort(t_stack *stack_a, t_stack *stack_b)
 {
 	int midvalue;
@@ -525,11 +580,15 @@ int		midpointsort(t_stack *stack_a, t_stack *stack_b)
 	counter = 0;
 	midvalue = find_midpoint(stack_a);
 	printf("THIS IS THE MIDVALUE: %d\n", midvalue);
+	visualize_stack(stack_a);
+	visualize_stack(stack_b);
+	printf("***********************************************************************************\n");
 	//check_top(stack_a, stack_b, midvalue);
 	//check_bottom(stack_a, stack_b, midvalue);
 	
 	while (counter < (initial_count / 2) || !check_if_smaller(stack_a, midvalue)) 
 	{
+		usleep(100000);
 		//checking top, if its bigger or equal we reverse it (only the when we have checked the botom)
 		while (midvalue <= stack_a->arr[stack_a->count - 1] && status != 0)
 		{
@@ -554,66 +613,222 @@ int		midpointsort(t_stack *stack_a, t_stack *stack_b)
 	return (counter);
 }
 
+int		check_top_second(int midvalue, t_stack *stack_b, int status)
+{
+	int rb_count;
+
+	rb_count = 0;
+	while (midvalue >= stack_b->arr[stack_b->count - 1] && status != 0 && stack_b->count > 0)
+	{
+		usleep(100000);	
+		//adding counter to keep  track
+		if (stack_b->chunk_number != 0)
+			rb_count++;
+		reverse(stack_b);
+		break ;
+	}
+	return (rb_count);
+}
+
+
+int check_top_first(int midvalue , t_stack *stack_a, t_stack *stack_b, int chunksize)
+{
+	int counter;
+
+	counter = 0;
+	while (midvalue < stack_b->arr[stack_b->count - 1] && counter <= (chunksize / 2) && stack_b->count > 0)
+	{
+		visualize_stack(stack_b);
+		if (!check_top2(stack_b))
+			swap(stack_b);
+		pop_element(stack_a, stack_b);				
+		counter++;
+		printf("COUNTER++\n");
+		stack_b->chunk_count--;
+		visualize_stack(stack_b);
+	}
+	return (counter);
+}
+
+int	check_bottom_chunk(int midvalue, t_stack *stack_a, t_stack *stack_b, int chunksize)
+{
+	int counter = 0;
+	int x = 1;
+	
+	while (midvalue < stack_b->arr[stack_b->chunk_count - chunksize + 1] && stack_b->count > 0)
+	{
+		printf("MIDVALUE ; %d\n", midvalue);
+		printf("CHUNKSIZE ; %d\n", chunksize);
+		printf("stack_b chunkcount ; %d\n", stack_b->chunk_count);
+		int p = 0;
+		while (p < chunksize - x)
+		{
+			usleep(100000);
+			reverse(stack_b);
+			p++;
+		}
+		
+		pop_element(stack_a, stack_b);
+		stack_b->chunk_count--;
+		x++;
+		counter++;
+		
+		while (p > 0)
+		{
+			reverse_reversed(stack_b);
+			p--;
+		}
+	}
+	return (counter);
+}
+
 void	restore_midpoint(t_stack *stack_a, t_stack *stack_b)
 {
 	int	chunksize;
 	int midvalue;
-
+	int status; 
+	int rb_count;
+	int	limit;
+	int counter;
 	//visualize_chunks(stack_b);
 	while (stack_b->chunk_count > 0)
 	{
+		status = 0;
+		rb_count = 0;
 		chunksize = count_chunk_size(stack_b);
+		
+		
 		printf("CHUNKSIZE: %d\n", chunksize);
+		limit = stack_b->chunk_count - chunksize;
+		printf("THIS IS LIMIT: %d\n", limit);
 		if (chunksize == 0)
 			break ;
 		else if (chunksize == 1)
+		{
 			pop_element(stack_a, stack_b);
+			stack_b->chunk_count--;
+		}	
 		else if (chunksize == 2)
 		{
 			if (check_if_sorted_2(stack_b))
 				swap(stack_b);
 			pop_element(stack_a, stack_b);
 			pop_element(stack_a, stack_b);
+			stack_b->chunk_count -= 2;
 		}
 		else
 		{
 			midvalue = find_midpoint_chunk(stack_b, chunksize);
 			printf("MIDVALUE: %d\n", midvalue);
-		
+			counter = 0;
+			//while (stack_b->chunk_count > 2)
+			//if (stack_b->chunk_count > 2)
+			//	while (limit <= stack_b->chunk_count && check_if_bigger(stack_b, midvalue, chunksize - counter))
+			//	{
+			printf("status : %d\n", status);
+			printf("counter : %d\n", counter);
+			//limit < stack_b->chunk_count
+			
+			while ((limit <= stack_b->chunk_count) && check_if_bigger(stack_b, midvalue, chunksize - counter) && stack_b->count > 0) 
+					//||(limit < stack_b->chunk_count && check_if_bigger(stack_b, midvalue, chunksize - counter)))
+			{
+				usleep(100000);
+				while (check_if_bigger(stack_b, midvalue, chunksize - counter) && stack_b->count > 0)
+				{
+					
+					rb_count += check_top_second(midvalue, stack_b, status);
+				
+					counter += check_top_first(midvalue, stack_a, stack_b, chunksize);
+				
+					counter += check_bottom_chunk(midvalue, stack_a, stack_b, chunksize);
+					status = 1;
+					printf("counter : %d\n", counter);
+					printf("chinksize : %d\n", chunksize);
+					printf("midvalue %d\n", midvalue);
+					printf("stack b count %d\n", stack_b->count);
+				}
+				//printf("counter : %d\n", counter);
+				//printf("chinksize : %d\n", chunksize);
+				
+				/*checking for 2 left in chunk
+				if (chunksize - counter < 3 )
+				{
+					if (stack_b->arr[stack_b->count - 1] < stack_b->arr[stack_b->count - 2])
+						swap(stack_b);
+					pop_element(stack_a, stack_b);
+					pop_element(stack_a, stack_b);
+					counter += 2;
+				}
+				*/
+				//status = 1;
+				visualize_stack(stack_a);
+				visualize_stack(stack_b);
+				
+				//if ()
+				//{
+					while (rb_count != 0)
+					{
+						reverse_reversed(stack_b);
+						rb_count--;
+					}
+					visualize_stack(stack_b);
+				//}
+				
+				if (chunksize - counter < 3)
+				{
+					if (stack_b->count == 1)
+						{
+							pop_element(stack_a, stack_b);
+							return ;
+						}
+					if (stack_b->arr[stack_b->count - 1] < stack_b->arr[stack_b->count - 2])
+						swap(stack_b);
+					pop_element(stack_a, stack_b);
+					pop_element(stack_a, stack_b);
+					stack_b->chunk_count -= 2;
+					counter += 2;
+				}
+				visualize_stack(stack_a);
+				visualize_stack(stack_b);
+				
+				midvalue = find_midpoint_chunk2(stack_b, chunksize - counter);
+				printf("[ new midvalue: %d ]\n", midvalue);
+				
+				/*
+				if (stack_b->count == 1 && check_if_sorted(stack_a))
+				{
+					pop_element(stack_a, stack_b);
+					break ;
+				}
+				*/
+			}
 			/*
-			pop_element(stack_a, stack_b);
-			reverse(stack_b);
-			pop_element(stack_a, stack_b);
-			reverse_reversed(stack_b);
-			pop_element(stack_a, stack_b);
+			while (rb_count != 0)
+			{
+				reverse_reversed(stack_b);
+				rb_count--;
+			}
 			*/
-
-			//DON'T FORGET TO CHECK IF ITS NOT THE LAST CHUNK, YOU HAVE TO COUNT THE Ra's and rra again.
-			visualize_stack(stack_a);
-			visualize_stack(stack_b);
-
+			printf("[ Chunk count: %d ]\n", stack_b->chunk_count);
+			printf("[ Chunk number: %d ]\n", stack_b->chunk_number);
+			printf("[ count: %d ]\n", stack_b->count);	
+			printf("[ chunksize: %d ]\n", chunksize);
+			printf("[ counter: %d ]\n", counter);
+			//midvalue = find_midpoint_chunk2(stack_b, chunksize - counter);
+			//printf("[ new midvalue: %d ]\n", midvalue);
 		}
+
+
+
 		visualize_chunks(stack_b);
-		//printf(RED"chunk_count : %d ### and chunksize : %d\n"RESET_COLOR, stack_b->chunk_count, chunksize);
-		stack_b->chunk_count -= chunksize;
-		//printf(RED"chunk_count : %d ### and chunksize : %d\n"RESET_COLOR, stack_b->chunk_count, chunksize);
-		printf("######################################################################################\n");
+		printf(RED"chunk_count : %d ### and chunksize : %d\n"RESET_COLOR, stack_b->chunk_count, chunksize);
+				//stack_b->chunk_count -= counter;
+		printf(RED"chunk_count : %d ### and chunksize : %d\n"RESET_COLOR, stack_b->chunk_count, chunksize);	
+		
 	}
-
-	//	chunksize = count_chunk_size(stack_b);
-	//	printf("CHUNKSIZE: %d\n", chunksize);
-		//else
-	//	{
-	//	midvalue = find_midpoint_chunk(stack_b, chunksize);
-	//	printf("MIDVALUE: %d\n", midvalue);
-		/*
-		chunksize = count_chunk_size(stack_b);
-		printf("CHUNKSIZE: %d\n", chunksize);
-		midvalue = find_midpoint_chunk(stack_b, chunksize);
-		printf("MIDVALUE: %d\n", midvalue);
-		*/
-	//	}	
-
+	//printf(RED"chunk_count : %d ### and chunksize : %d\n"RESET_COLOR, stack_b->chunk_count, chunksize);
+	printf("######################################################################################\n");
+	//	}
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -677,6 +892,7 @@ int	main(int argc, char *argv[])
 	stack_b->chunk_number = 0;
 	while (stack_a->count > 2)
 	{
+		usleep(100000);
 		size_of_chunk = midpointsort(stack_a, stack_b);
 		set_chunks(stack_b, size_of_chunk);
 		//stack_b->chunk_number++;
@@ -689,11 +905,13 @@ int	main(int argc, char *argv[])
 	if(!check_if_sorted(stack_a))
 		swap(stack_a);
 	//printf("%d\n", find_midpoint(stack_a));
-	printf(GREEN"OPERATIONS : %d\n"RESET_COLOR, operations);
+	//printf(GREEN"OPERATIONS : %d\n"RESET_COLOR, operations);
 	restore_midpoint(stack_a, stack_b);	
 	//visualize_chunks(stack_b);
 	visualize_stack(stack_a);
 	visualize_stack(stack_b);
+	check_if_sorted(stack_a);
+	printf(GREEN"OPERATIONS : %d\n"RESET_COLOR, operations);
 	/*
 	printf("this is the first chunksize %d\n", count_chunk_size(stack_b));
 	printf("this is the second chunksize %d\n", count_chunk_size(stack_b));
